ff69bd80f6bd676d70d1a3d9214e3b39d0c102d0
unknown <moisesmolinacrispin6@gmail.com>
2026-09-21 17:55:24 -0500

    Mejorar responsive y corregir carga de trabajos

--- src/main/java/com/portafolio/moises/repository/TrabajoRepository.java
+++ src/main/java/com/portafolio/moises/repository/TrabajoRepository.java
@@ -2,6 +2,7 @@ package com.portafolio.moises.repository;
 
 import com.portafolio.moises.model.Trabajo;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -10,10 +11,22 @@ public interface TrabajoRepository
         extends JpaRepository<Trabajo, Long> {
 
     // =========================================
-    // OBTENER TRABAJOS DE UNA SEMANA
-    // DEL MÁS ANTIGUO AL MÁS NUEVO
+    // OBTENER TODOS LOS TRABAJOS
+    // CARGANDO TAMBIÉN SU SEMANA
     // =========================================
 
+    @Override
+    @EntityGraph(attributePaths = "semana")
+    List<Trabajo> findAll();
+
+
+    // =========================================
+    // OBTENER TRABAJOS DE UNA SEMANA
+    // DEL MÁS ANTIGUO AL MÁS NUEVO
+    // CARGANDO TAMBIÉN SU SEMANA
+    // =========================================
+
+    @EntityGraph(attributePaths = "semana")
     List<Trabajo> findBySemanaIdOrderByIdAsc(
             Long semanaId
     );
